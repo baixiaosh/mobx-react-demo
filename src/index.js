9107d393b3259d9fe9e661d1b60f07fb540f368d
baixiaosh <782179350@qq.com>
2018-05-04 14:20:41 +0800

    集成UI库

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-<<<<<<< 10f81f39d49b5b6a4bed713f883797d0086c42c0
-=======
 import { LocaleProvider } from "antd"
 import zh_CN from "antd/lib/locale-provider/zh_CN"
->>>>>>> 添加UI库,ant-design
 import { useStrict } from 'mobx'
 import { Provider } from 'mobx-react'
 import { HashRouter as Router, Route } from 'react-router-dom'
@@ -19,14 +16,6 @@ useStrict(true)
 
 ReactDOM.render(
   <Provider {...stores}>
-<<<<<<< 10f81f39d49b5b6a4bed713f883797d0086c42c0
-    <Router >
-    	<div>
-	    	<Route exact path="/" component={App}/>
-	    	<Route path="/login" component={Login}/>
-    	</div>
-    </Router>
-=======
     <LocaleProvider locale={zh_CN}>
 	    <Router >
 	    	<div>
@@ -35,7 +24,6 @@ ReactDOM.render(
 	    	</div>
 	    </Router>
     </LocaleProvider>
->>>>>>> 添加UI库,ant-design
   </Provider>,
   document.getElementById('root')
 )
--- src/routes/App.js
+++ src/routes/App.js
@@ -15,17 +15,10 @@ class App extends React.Component {
     const store = new TodoListModel()
     store.addTodo("Get Coffee")
     store.addTodo("Write simpler code")
-<<<<<<< 10f81f39d49b5b6a4bed713f883797d0086c42c0
-		login({ username: '123', password: 'demo' }).then((res)=>{
-			console.log(res)
-			store.addTodo(res.msg)
-		})
-=======
 		// login({ username: '123', password: 'demo' }).then((res)=>{
 		// 	console.log(res)
 		// 	store.addTodo(res.msg)
 		// })
->>>>>>> 添加UI库,ant-design
     return <TodoList store={store} />
   }
 }
